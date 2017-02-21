ef78bd1e6e73c7d77e5d8a69dbfabff9ca2043da
sb4aa4bs <bharath.softy@gmail.com>
2017-02-20 18:59:06 -0700

    Added Base Project Template

---
+++ SpringPropertiesPlaceHolderExamples/src/main/java/our/abs2/common/App.java
@@ -0,0 +1,13 @@
+package our.abs2.common;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
